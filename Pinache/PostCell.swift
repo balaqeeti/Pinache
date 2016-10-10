12cb7d84a174fb5b4e2d8d325f6052371c27c4c9
jettraines <jettraines21@gmail.com>
2016-10-10 14:49:59 -0700

    Feed UI is done

---
+++ Pinache/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  Pinache
+//
+//  Created by admin on 10/10/16.
+//  Copyright © 2016 Jett Raines. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
