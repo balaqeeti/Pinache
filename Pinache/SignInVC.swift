58dbf967a5f6a9006c5a2829f1801ebcd960f691
jettraines <jettraines21@gmail.com>
2016-10-10 09:56:58 -0700

    Finished basic Facebook Authentication

--- Pinache/SignInVC.swift
+++ Pinache/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,35 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookButtonTapped(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("JETT: Unable to authenticate with facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("JETT: User Cancelled Facebook Authentication")
+            } else {
+                print("JETT: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+                
+            }
+        }
+        
+    }
 
+    func firebaseAuth(_ credential: FIRAuthCredential){
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("JETT: Unable to authenticate with Firebase")
+            } else {
+                print("JETT: Successfully Authenticated with Firebase")
+            }
+        })
+    }
+    
+    
 }
 
