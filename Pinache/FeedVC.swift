ae5cec30bc80df89468e3c6b2c0123d8377b86ac
jettraines <jettraines21@gmail.com>
2016-10-11 13:26:22 -0700

    Images are uploadable to Firebase

--- Pinache/FeedVC.swift
+++ Pinache/FeedVC.swift
@@ -16,9 +16,13 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     
     @IBOutlet weak var imageAdd: UIImageView!
     
+   @IBOutlet weak var captionField: FancyField!
+   
+    
     var posts = [Post]()
     var imagePicker: UIImagePickerController!
     static var imageCache: NSCache<NSString, UIImage> = NSCache()
+    var imageSelected = false
     
     
     
@@ -86,6 +90,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
             imageAdd.image = image
+            imageSelected = true
         } else {
             print("JETT: A valid image wasn't selected" )
         }
@@ -96,6 +101,39 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     
         present(imagePicker, animated: true, completion: nil)
     }
+   
+    
+    
+    @IBAction func postBtnTapped(_ sender: AnyObject) {
+        guard let caption = captionField.text, caption != "" else {
+            print("JETT: Caption must be entered")
+            return
+        }
+        
+        guard let img = imageAdd.image, imageSelected == true else {
+            print("JETT: An image must be selected")
+            return
+        }
+    
+        if let imgData = UIImageJPEGRepresentation(img, 0.2){
+            
+            let imgUid = NSUUID().uuidString
+            let metadata = FIRStorageMetadata()
+            metadata.contentType = "image/jpeg"
+            DataService.ds.REF_POST_IMAGES.child(imgUid).put(imgData, metadata: metadata) { (metadata, error) in
+                if error != nil {
+                    print("JETT: Unable to upload image to Firebase storage")
+                } else {
+                    print("JETT: Successfully uploaded image to firebase storage")
+                    let downloadURL = metadata?.downloadURL()?.absoluteString
+                    
+                }
+            }
+            
+        }
+    }
+    
+    
     @IBAction func signOutTapped(_ sender: AnyObject) {
         let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         print("JETT: \(keychainResult)")
