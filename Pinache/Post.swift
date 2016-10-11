7f6fc0938d96cd0ce8278f13a3f98c2857873876
jettraines <jettraines21@gmail.com>
2016-10-10 19:47:45 -0700

    Firebase GET completed

---
+++ Pinache/Post.swift
@@ -0,0 +1,57 @@
+//
+//  Post.swift
+//  Pinache
+//
+//  Created by admin on 10/10/16.
+//  Copyright © 2016 Jett Raines. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    private var _caption: String!
+    private var _imageUrl: String!
+    private var _likes: Int!
+    private var _postKey: String!
+    
+    var caption: String {
+        return _caption
+    }
+    
+    var imageUrl: String {
+        return _imageUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var postKey: String {
+        return _postKey
+    }
+    
+    
+    init(caption: String, imageURL: String, likes: Int){
+        self._caption = caption
+        self._imageUrl = imageURL
+        self._likes = likes
+    }
+    
+    
+    init(postKey: String, postData: Dictionary<String, AnyObject>) {
+        self._postKey = postKey
+        
+        if let caption = postData["caption"] as? String {
+            self._caption = caption
+        }
+        if let imageUrl = postData["imageURL"] as? String {
+            self._imageUrl = imageUrl
+        }
+        if let likes = postData["likes"] as? Int {
+            self._likes = likes
+        }
+        
+        
+        
+    }
+}
